9dc6583713b98d14fc1029799a7a2f301f87e742
kishanth20702-sudo <kishanth20702@gmail.com>
2026-09-19 09:45:22 +0530

    my fisrt push after sometime

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello world");
+console.log(`i like pizza`);
+
+//document.getElementById("myh1").textContent = `Hello`;
+//document.getElementById("myP1").textContent = `i like pizza`;
+//window.alert(`this is an alert`)
+//window.alert(`i like pizza`)
+//this is a comment
+
+let x;
+x=100;
+
+console.log(x);
+let age = 25;
+console.log(`age`);
+
+console.log(`you are ${age} years`)
+console.log(typeof age );
