2f4a0b15094bccb7f918dc1172670f36c1058259
kishanth20702-sudo <kishanth20702@gmail.com>
2026-09-19 20:57:26 +0530

    second commit of the day

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ console.log(`i like pizza`);
 //window.alert(`i like pizza`)
 //this is a comment
 
-let x;
+/*let x;
 x=100;
 
 console.log(x);
@@ -16,3 +16,25 @@ console.log(`age`);
 
 console.log(`you are ${age} years`)
 console.log(typeof age );
+
+
+let day = true;
+colsole.log(`it is ${day}`)*/
+let username;
+
+
+
+/*username = window.prompt("what is your user name?");
+
+document.getElementById("myh1").textContent=`Hello ${username}`;
+
+console.log(username)*/
+
+let user;
+
+document.getElementById("mysubmit").onclick = function(){
+    user =  document.getElementById("user").value
+    console.log(user);
+    document.getElementById("myh1").textContent=`Hello ${user}`;
+}
+
